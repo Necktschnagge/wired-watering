1d3236657753851a9033adc2fb6d92018a2174f5
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-26 21:57:56 +0200

    update maya internet script

--- maya/script/maya_connect_internet_usb0.sh
+++ maya/script/maya_connect_internet_usb0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-timeout 60s dhclient usb0
+(timeout 60s dhclient usb0) && echo "Connecting to internet via usb0 tethering successful." || echo "Connecting to internet via usb0 tethering FAILED"
 
 exit #will exit with status of last command
