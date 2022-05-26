3885fcf95093edc50d76a2f7a21afe5efe0f9692
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-26 22:07:30 +0200

    maya os init script

---
+++ maya/script/maya_os_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#git init
+git config --global credential.helper store
+git config --global user.name "Maya Server"
+git config --global user.email "maya.watering@gmail.com"
+
+
+exit #will exit with status of last command
