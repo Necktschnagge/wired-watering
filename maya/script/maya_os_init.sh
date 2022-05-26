53f4b59ddb53c24e72869d312f93b97fcf8ef41b
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-26 22:24:57 +0200

    add some server setup steps

--- maya/script/maya_os_init.sh
+++ maya/script/maya_os_init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+  echo >&2 "Error: script not running as root or with sudo! Exiting..."
+  exit 1
+fi
+
 #git init
 git config --global credential.helper store
 git config --global user.name "Maya Server"
 git config --global user.email "maya.watering@gmail.com"
 
+exit
+
+#required software
+apt-get install cmake
 
 exit #will exit with status of last command
