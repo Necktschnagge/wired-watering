36062c009b00dac5ad65020f63ba6987367ec2c6
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-26 22:28:17 +0200

    enhancement of os init script

--- maya/script/maya_os_init.sh
+++ maya/script/maya_os_init.sh
@@ -5,14 +5,16 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-#git init
+echo "Installing git..."
+apt-get install git
+
+echo "Configuring git..."
 git config --global credential.helper store
 git config --global user.name "Maya Server"
 git config --global user.email "maya.watering@gmail.com"
 
-exit
-
-#required software
+echo "Installing CMake..."
 apt-get install cmake
+
 
 exit #will exit with status of last command
