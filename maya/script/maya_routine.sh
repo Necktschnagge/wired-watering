829c68f46dcdc05d37ac43dad44b414493dc39e9
Maya Server <maya.watering@gmail.com>
2022-05-27 19:56:23 +0200

    run git commands as normal user

--- maya/script/maya_routine.sh
+++ maya/script/maya_routine.sh
@@ -24,35 +24,35 @@ do
 	(
 		echo "Starting with run #${run_counter}..."; \
 		echo "Cleaning working directory..."; \
-		git add -u; git add *; \
-		git status; \
-		git reset --hard; \
+		sudo -u mayadm git add -u; git add *; \
+		sudo -u mayadm git status; \
+		sudo -u mayadm git reset --hard; \
 		echo "Cleaning working directory   ...DONE!"; \
 		echo "Loading and updating working branch..."; \
-		git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}; \
+		sudo -u mayadm git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}; \
 		(
-			git pull && echo "Pulling ${BRANCH_TO_LOAD_AS_WORKING_BRANCH} successfully!"
+			sudo -u mayadm git pull && echo "Pulling ${BRANCH_TO_LOAD_AS_WORKING_BRANCH} successfully!"
 		) || 
 		(
 			echo "Pulling ${BRANCH_TO_LOAD_AS_WORKING_BRANCH} failed! Resetting to last commit on local branch ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}" && git git reset --hard
 		) \
-	) | tee logs/01.log
+	) | sudo -u mayadm tee logs/01.log
 	echo "Switching to working branch..."
-	git switch -c ${LOCAL_WORKING_BRANCH}
+	sudo -u mayadm git switch -c ${LOCAL_WORKING_BRANCH}
 	
-	git submodule update --init --recursive
-	./up-unix-create-project.sh
-	./ub-unix-build.sh
-	./ur-unix-run.sh
+	sudo -u mayadm git submodule update --init --recursive
+	sudo -u mayadm ./up-unix-create-project.sh
+	sudo -u mayadm ./ub-unix-build.sh
+	sudo -u mayadm ./ur-unix-run.sh
 	
 	
 	echo "Cleaning git working directory..."
-	git add -u; git add *; \
-	git status; \
-	git reset --hard; \
+	sudo -u mayadm git add -u; sudo -u mayadm git add *; \
+	sudo -u mayadm git status; \
+	sudo -u mayadm git reset --hard; \
 	echo "Deleting local working branch..."
-	git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}
-	git branch -D ${LOCAL_WORKING_BRANCH}
+	sudo -u mayadm git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}
+	sudo -u mayadm git branch -D ${LOCAL_WORKING_BRANCH}
 
 	echo "$Finished run #{run_counter}!"
 	date
