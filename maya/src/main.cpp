fe785bf0019c77f7ebeff71eb010f90092666ac2
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-28 12:38:40 +0200

    commit demo for http request and system time

--- maya/src/main.cpp
+++ maya/src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <iostream>
+#include <chrono>
 
 static const std::string IP_ADDRESS_PUMP_SERVER_MAYSON { "192.168.1.10" };
 static const std::string IP_ADDRESS_VALVE_SERVER_JAMES { "192.168.1.20" };
@@ -46,8 +47,24 @@ int main(int argc, char** argv){
 	ping(IP_ADDRESS_PUMP_SERVER_MAYSON);
 	ping(TEST_ADRESS_PING_FAIL);
 
-	cpr::Response resp;
+	cpr::Response r = cpr::Get(
+		cpr::Url{ "https://api.github.com/repos/libcpr/cpr/contributors" },
+		//cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
+		cpr::Parameters{ {"anon", "true"}, {"key", "value"} }
+	);
+	r.status_code;                  // 200
+	r.header["content-type"];       // application/json; charset=utf-8
+	r.text;
 
+	std::cout << r.text << std::endl;
+	std::cout << r.status_code << std::endl;
+
+	
+	auto time_since_0 = std::chrono::system_clock::now().time_since_epoch();
+	
+	std::cout << time_since_0.count() << std::endl;
+	std::cout << time_since_0.count()/10000000 << std::endl;
+	std::cout << (time_since_0.count()/600000000) % (24*60)<< std::endl; // hour of the day in UTC (we are 2 hours ahead.)
 
 	return 0;
 }
