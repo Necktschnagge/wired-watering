53897c8c61ff949add369988513674ba45f96fe1
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-29 08:49:04 +0200

    all_stuff

--- maya/script/maya_routine.sh
+++ maya/script/maya_routine.sh
@@ -12,7 +12,7 @@ BRANCH_TO_LOAD_AS_WORKING_BRANCH="nchain"
 BRANCH_TO_LOAD_AS_FALLBACK_BRANCH="productive-fallback"
 
 #current_sleep_time_s=900
-current_sleep_time_s=9
+current_sleep_time_s=900
 
 run_counter=0
 
@@ -28,6 +28,7 @@ do
 		sudo -u mayadm git status; \
 		sudo -u mayadm git reset --hard; \
 		echo "Cleaning working directory   ...DONE!"; \
+		sleep 5s
 		echo "Loading and updating working branch..."; \
 		sudo -u mayadm git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}; \
 		(
@@ -37,9 +38,10 @@ do
 			echo "Pulling ${BRANCH_TO_LOAD_AS_WORKING_BRANCH} failed! Resetting to last commit on local branch ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}" && git git reset --hard
 		) \
 	) | sudo -u mayadm tee logs/01.log
+	sleep 10s
 	echo "Switching to working branch..."
 	sudo -u mayadm git switch -c ${LOCAL_WORKING_BRANCH}
-	sleep 20s
+	sleep 100s #let the servers connect to wifi!
 	
 	sudo -u mayadm git submodule update --init --recursive
 	sudo -u mayadm ./up-unix-create-project.sh
@@ -55,13 +57,11 @@ do
 	sudo -u mayadm git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}
 	sudo -u mayadm git branch -D ${LOCAL_WORKING_BRANCH}
 
-	echo "$Finished run #{run_counter}!"
+	echo "Finished run ${run_counter}!"
 	date
 	echo "Going to sleep..."
 	sleep 10s
 	rtcwake -m mem -s ${current_sleep_time_s}
 	echo "Waking up from sleep..."
 	date
-	#debug:
-	exit
 done
--- maya/src/main.cpp
+++ maya/src/main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <chrono>
 
+#include <fstream>
+
 static const std::string IP_ADDRESS_PUMP_SERVER_MAYSON{ "192.168.1.10" };
 static const std::string IP_ADDRESS_VALVE_SERVER_JAMES{ "192.168.1.20" };
 static const std::string IP_ADDRESS_VALVE_SERVER_LUCAS{ "192.168.1.21" };
@@ -36,6 +38,110 @@ bool ping(const std::string& ip_address) {
 	}
 }
 
+int64_t get_seconds_since_epoch() {
+	auto time_since_0 = std::chrono::system_clock::now().time_since_epoch();
+
+	std::cout << time_since_0.count() << std::endl;
+	auto seconds = time_since_0.count() / 10000000;
+#ifdef __linux__
+	seconds /= 100;
+#endif
+	std::cout << seconds << std::endl; //two more 0 on linux
+	std::cout << (seconds / 60) % (24 * 60) << std::endl; // hour of the day in UTC (we are 2 hours ahead.)
+
+	return seconds;
+}
+
+static uint8_t james_valves{ 0 };
+
+void send_mayson(bool on) {
+	std::cout << "send pump relay\n" << on << std::endl;
+	std::string url{ "http://" };
+	url += IP_ADDRESS_PUMP_SERVER_MAYSON;
+	url += ":80/status";
+	cpr::Response r = cpr::Get(
+		cpr::Url{ url },
+		//cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
+		cpr::Parameters{ {"auto", on ? "on" : "off" }}
+	);
+	//r.status_code;                  // 200
+	//r.header["content-type"];       // application/json; charset=utf-8
+	//r.text;
+
+	std::cout << r.text << std::endl;
+	std::cout << r.status_code << std::endl;
+
+}
+
+
+void send_james(uint8_t valves) {
+	std::cout << "send valves james\n";
+	std::string url{ "http://" };
+	url += IP_ADDRESS_VALVE_SERVER_JAMES;
+	url += ":80/status";
+	cpr::Response r = cpr::Get(
+		cpr::Url{ url },
+		//cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
+		cpr::Parameters{ {"valves", std::to_string(valves) } }
+	);
+	//r.status_code;                  // 200
+	//r.header["content-type"];       // application/json; charset=utf-8
+	//r.text;
+
+	std::cout << r.text << std::endl;
+	std::cout << r.status_code << std::endl;
+
+}
+
+void apply_james(uint8_t valves_new) {
+	uint8_t current_james{ james_valves };
+	uint8_t turn_off = james_valves & (~valves_new);
+	uint8_t turn_on = (~james_valves) & valves_new;
+	for (uint8_t i = 0; i < 8; ++i) {
+		uint8_t pos = 1 << i;
+		if ((turn_off & pos) || (turn_on & pos)) {
+			current_james &= ~pos;
+			current_james |= pos & valves_new;
+			send_james(current_james);
+			std::this_thread::sleep_for(std::chrono::milliseconds(400));
+		}
+	}
+
+
+	james_valves = valves_new;
+	send_james(james_valves);
+};
+
+void watering(const int64_t& seconds_since_epoch) {
+	const int64_t minutes_since_epoch{ seconds_since_epoch / 60 };
+	const int64_t hours_since_epoch{ minutes_since_epoch / 60 };
+	const int64_t days_since_epoch{ hours_since_epoch / 24 };
+
+
+	if (days_since_epoch % 2) {
+		
+		//pumpe an
+		send_mayson(true);
+		
+		auto start_watering_1 = get_seconds_since_epoch();
+		apply_james(0b00000011);
+		while (get_seconds_since_epoch() < start_watering_1 + 60 * 20) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		apply_james(0b00000000);
+		
+		auto start_watering_2 = get_seconds_since_epoch();
+		apply_james(0b00001100);
+		while (get_seconds_since_epoch() < start_watering_2 + 60 * 20) {
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		apply_james(0b00000000);
+		
+		send_mayson(false);
+		//pumpe aus
+	}
+}
+
 
 int main(int argc, char** argv) {
 
@@ -59,16 +165,50 @@ int main(int argc, char** argv) {
 	std::cout << r.text << std::endl;
 	std::cout << r.status_code << std::endl;
 
+	auto seconds = get_seconds_since_epoch();
+	auto minutes_since_epoch = (seconds / 60);
+	auto minute_of_the_day = (seconds / 60) % (24 * 60);
 
-	auto time_since_0 = std::chrono::system_clock::now().time_since_epoch();
+	int64_t previous_timestamp = 0;
+	{
+		std::ifstream is;
+		is.open("../../artifacts/timestamp.txt");
+		if (is.is_open()) {
+			std::string line;
+			std::getline(is, line);
+			try {
+				previous_timestamp = std::stoll(line);
+			}
+			catch (...) {
 
-	std::cout << time_since_0.count() << std::endl;
-	auto seconds = time_since_0.count() / 10000000;
-#ifdef __linux__
-	seconds /= 100;
-#endif
-	std::cout << seconds << std::endl; //two more 0 on linux
-	std::cout << (seconds / 60 )% (24 * 60) << std::endl; // hour of the day in UTC (we are 2 hours ahead.)
+			}
+		}
+		is.close();
+	}
+	std::cout << "previous time_stamp:   " << previous_timestamp << std::endl;
+
+	if (
+		(minute_of_the_day > (4 - 2) * 60) // 4:00 // -2 == UTC 
+		&& (minute_of_the_day < (7 - 2) * 60) // 7:00
+		&& (previous_timestamp + 3 * 60 + 1 < minutes_since_epoch) // 3 hours gone since last watering
+		)
+	{
+	} {
+		// save last timestamp:
+		std::ofstream s;
+		s.open("../../artifacts/timestamp.txt");
+		if (s.good()) {
+			s << minutes_since_epoch << std::endl;
+			std::cout << "Wrote to timestamp.txt" << std::endl;
+
+			watering(seconds);
+
+
+		}
+		else {
+			std::cout << "error timestamp writing";
+		}
+	}
 
 	return 0;
 }
