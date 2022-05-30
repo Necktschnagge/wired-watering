d80791845e92a8f15a2c2aca043be4ff8cfb1142
Maximilian Starke <maximilian.starke@medionmail.com>
2022-05-30 14:53:13 +0200

    stuff

--- maya/script/maya_routine.sh
+++ maya/script/maya_routine.sh
@@ -22,13 +22,32 @@ while true
 do
 	let run_counter++
 	(
-		echo "Starting with run #${run_counter}..."; \
-		echo "Cleaning working directory..."; \
-		sudo -u mayadm git add -u; git add *; \
+		echo "====================================================================================================="; \
+		echo "====================================================================================================="; \
+		echo "===     Starting with run #${run_counter}..."; \
+		echo "====================================================================================================="; \
+		echo "%%%%%%%%%%     [1] Cleaning working directory..."; \
+		echo "> sudo -u mayadm git status;"; \
 		sudo -u mayadm git status; \
+		echo "> sudo -u mayadm git add -u; git add *;"; \
+		sudo -u mayadm git add -u; git add *; \
+		echo "> sudo -u mayadm git status;"; \
+		sudo -u mayadm git status; \
+		echo "> sudo -u mayadm git ####...."
+		#check if changes.
+		#if so: new branch, commit, 
+		echo "> sudo -u mayadm git update-index --refresh && sudo -u mayadm git diff-index --quiet HEAD --"
+		sudo -u mayadm git update-index --refresh && sudo -u mayadm git diff-index --quiet HEAD --
+
+		echo "> sudo -u mayadm git reset --hard;"; \
 		sudo -u mayadm git reset --hard; \
 		echo "Cleaning working directory   ...DONE!"; \
+		echo "%%%%%%%%%%     [1] Cleaning working directory   ...DONE!"; \
+		echo "====================================================================================================="; \
 		sleep 5s
+		echo "====================================================================================================="; \
+		echo "%%%%%%%%%%     [2] Loading source branch, pulling update from origin..."; \
+		###
 		echo "Loading and updating working branch..."; \
 		sudo -u mayadm git checkout ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}; \
 		(
@@ -38,7 +57,10 @@ do
 			echo "Pulling ${BRANCH_TO_LOAD_AS_WORKING_BRANCH} failed! Resetting to last commit on local branch ${BRANCH_TO_LOAD_AS_WORKING_BRANCH}" && git git reset --hard
 		) \
 	) | sudo -u mayadm tee logs/01.log
+	echo "%%%%%%%%%%     [2] Loading source branch, pulling update from origin   ...DONE!"; \
+	echo "====================================================================================================="; \
 	sleep 10s
+	echo "%%%%%%%%%%     [2] Loading source branch, pulling and switching to new branch for maya_routine..."; \
 	echo "Switching to working branch..."
 	sudo -u mayadm git switch -c ${LOCAL_WORKING_BRANCH}
 	sleep 10s #let the servers connect to wifi!
