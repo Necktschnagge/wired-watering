54f4c0eaa7ed50ef24cbad4d4dbb9272942b470b
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-14 14:00:45 +0200

    try this

--- maya/script/maya_routine.sh
+++ maya/script/maya_routine.sh
@@ -26,8 +26,6 @@ fi
 
 	let run_counter++
 log_file_name_prefix="${logs_path}/${run_counter}--$(date +%Y-%m-%d--%Hh%M)--"
-log_commit_message="maya logs:   $(date +%Y-%m-%d\ \ \ %H:%M)"
-
 	(
 		echo "====================================================================================================="; \
 		echo "====================================================================================================="; \
@@ -235,7 +233,7 @@ log_commit_message="maya logs:   $(date +%Y-%m-%d\ \ \ %H:%M)"
 		#sudo -u mayadm git add -u
 		sudo -u mayadm git add ../artifacts/logs/*.log -f
 		sudo -u mayadm git add ../artifacts/logs/*.sh -f
-		sudo -u mayadm git commit -m "${log_commit_message}"
+		sudo -u mayadm git commit -m "upload logs"
 		sudo -u mayadm git push
 		sudo -u mayadm git checkout maya-productive
 
