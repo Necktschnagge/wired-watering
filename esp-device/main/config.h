97605b4be2fe1cbce70c4af26f5fe1035c55eeef
Maximilian Starke <maximilian.starke@medionmail.com>
2022-06-26 00:22:11 +0200

    update config.h for esp device

--- esp-device/main/config.h
+++ esp-device/main/config.h
@@ -2,20 +2,43 @@
 
 #include "secret_config.h"
 
+
+#define PUMP_RELAY_MAYSON
+//#define VALVE_SERVER_JAMES
+//#define VALVE_SERVER_LUCAS
+
+#ifdef PUMP_RELAY_MAYSON
+
+static const char* string_server_name = "pump-relay-mayson";
+static const char* TAG = "pump-server";
+
 #define IP_CLIENT_CONFIG \
 IP4_ADDR(&sta_ip.ip, 192, 168, 1, 10); \
 IP4_ADDR(&sta_ip.gw, 192, 168, 1, 1); \
 IP4_ADDR(&sta_ip.netmask, 255, 255, 255, 0);
 
-#define PUMP_RELAY_MAYSON
-//#define VALVE_SERVER_JAMES
-
-#ifdef PUMP_RELAY_MAYSON
-static const char* string_server_name = "pump-relay-mayson";
-static const char* TAG = "pump-server";
 #endif // PUMP_RELAY_MAYSON
 
 #ifdef VALVE_SERVER_JAMES
+
 static const char* string_server_name = "valve-server-james";
 static const char* TAG = "valve-server";
-#endif // PUMP_RELAY_MAYSON
+
+#define IP_CLIENT_CONFIG \
+IP4_ADDR(&sta_ip.ip, 192, 168, 1, 20); \
+IP4_ADDR(&sta_ip.gw, 192, 168, 1, 1); \
+IP4_ADDR(&sta_ip.netmask, 255, 255, 255, 0);
+
+#endif // VALVE_SERVER_JAMES
+
+#ifdef VALVE_SERVER_LUCAS
+
+static const char* string_server_name = "valve-server-lucas";
+static const char* TAG = "valve-server";
+
+#define IP_CLIENT_CONFIG \
+IP4_ADDR(&sta_ip.ip, 192, 168, 1, 21); \
+IP4_ADDR(&sta_ip.gw, 192, 168, 1, 1); \
+IP4_ADDR(&sta_ip.netmask, 255, 255, 255, 0);
+
+#endif // VALVE_SERVER_LUCAS
