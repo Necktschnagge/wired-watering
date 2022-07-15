d8895d627fb0cc220c50a1da47bff0340d5f9508
Maximilian Starke <maximilian.starke@medionmail.com>
2022-07-15 23:53:44 +0200

    add file for each esp device, build for each device

--- esp-device/main/select_device_mayson.h
+++ esp-device/main/select_device_mayson.h
@@ -1,9 +1,14 @@
 
-/*
+
 #define PUMP_RELAY_MAYSON
-#define VALVE_SERVER_FELIX
-*/
-#define VALVE_SERVER_JAMES
+
 /*
+
+the other ones:
+
+#define VALVE_SERVER_FELIX
+#define VALVE_SERVER_JAMES
 #define VALVE_SERVER_LUCAS
+
 */
+
