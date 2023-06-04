439869e8a4febe3cc4266979d4a1f02528787ee4
Maximilian Starke <maximilian.starke@medionmail.com>
2023-06-04 12:41:44 +0200

    valve station classes and views

--- maya/src/main.cpp
+++ maya/src/main.cpp
@@ -4,6 +4,7 @@
 
 #include "cpr/cpr.h"
 
+#include <vector>
 #include <string>
 #include <iostream>
 #include <chrono>
@@ -11,6 +12,33 @@
 #include <fstream>
 #include <optional>
 
+namespace CONF {
+
+
+	// server names / device names:
+	[[maybe_unused]] inline static const std::string FELIX{ "Felix"};
+	[[maybe_unused]] inline static const std::string JAMES{ "James"};
+	[[maybe_unused]] inline static const std::string LUCAS{ "Lucas"};
+	[[maybe_unused]] inline static const std::string MAYSON{ "Mayson"};
+
+
+	// valve patch infos:
+	[[maybe_unused]] inline static const std::string FELIX_VALVE_1_LABEL{ "Eiben Klee" };
+	[[maybe_unused]] inline static const std::string FELIX_VALVE_2_LABEL{ "Mara Sabine" };
+
+	[[maybe_unused]] inline static const std::string JAMES_VALVE_1_LABEL{ "Karotten" };
+	[[maybe_unused]] inline static const std::string JAMES_VALVE_2_LABEL{ "Gurken" };
+	[[maybe_unused]] inline static const std::string JAMES_VALVE_3_LABEL{ "Tomaten" };
+	[[maybe_unused]] inline static const std::string JAMES_VALVE_4_LABEL{ "-frei-" };
+
+	[[maybe_unused]] inline static const std::string LUCAS_VALVE_1_LABEL{ "Erdbeerren Flieder" };
+	[[maybe_unused]] inline static const std::string LUCAS_VALVE_2_LABEL{ "Heidelbeeren" };
+	[[maybe_unused]] inline static const std::string LUCAS_VALVE_3_LABEL{ "Kartofffeln" };
+
+
+	// ip addresses:
+
+}
 
 [[maybe_unused]] static const std::string IP_ADDRESS_PUMP_SERVER_MAYSON{ "192.168.1.10" };
 
@@ -34,12 +62,11 @@
 [[maybe_unused]] static constexpr uint8_t FELIX_VALVE_1{ 0b00000001 };
 [[maybe_unused]] static constexpr uint8_t FELIX_VALVE_2{ 0b00000010 };
 
-[[maybe_unused]] inline static const std::string FELIX_VALVE_1_LABEL{ "Eiben Klee" };
-[[maybe_unused]] inline static const std::string FELIX_VALVE_2_LABEL{ "Mara Sabine" };
 
-[[maybe_unused]] static constexpr uint8_t JAMES_GURKE_ERBSE{ JAMES_VALVE_1 };
-[[maybe_unused]] static constexpr uint8_t JAMES_TOMATE{ JAMES_VALVE_2 };
-[[maybe_unused]] static constexpr uint8_t JAMES_BOHNEN{ JAMES_VALVE_4 };
+[[maybe_unused]] static constexpr uint8_t JAMES_KAROTTEN{ JAMES_VALVE_1 };
+[[maybe_unused]] static constexpr uint8_t JAMES_GURKEN{ JAMES_VALVE_2 };
+[[maybe_unused]] static constexpr uint8_t JAMES_TOMATEN{ JAMES_VALVE_3 };
+[[maybe_unused]] static constexpr uint8_t JAMES_FREI{ JAMES_VALVE_4 };
 
 //[[maybe_unused]] static constexpr uint8_t LUCAS_KAROTTEN{ LUCAS_VALVE_1 };
 //[[maybe_unused]] static constexpr uint8_t LUCAS_ERDBEEREN{ LUCAS_VALVE_2 };
@@ -132,68 +159,6 @@ public:
 };
 
 
-
-namespace k1 {
-
-
-	class valve {
-		std::string ip_address;
-		std::string label;
-		uint8_t index_intra_valve_sation;
-
-	public:
-		valve(const std::string& _ip_address, const std::string& _label, uint8_t _valve_bit_mask) : ip_address(_ip_address), label (_label), index_intra_valve_sation(_valve_bit_mask){}
-
-	};
-
-	class valve_station {
-		std::vector<valve> valves;
-
-		friend class landscape;
-
-		valve_station(std::vector<valve>&& valves) : valves(std::move(valves)) {
-
-		}
-		
-	public:
-
-	};
-
-
-
-	class landscape {
-		std::vector<valve_station> stations;
-		static std::optional<landscape> singleton_instance;
-
-
-		landscape() {
-			standard_logger()->info("Creating Landscape...");
-
-			// Felix:
-			auto felix_vector = std::vector<valve>{valve(IP_ADDRESS_VALVE_SERVER_FELIX, FELIX_VALVE_1_LABEL, 1<<0)};
-			stations.emplace_back(std::move(felix_vector));
-
-		}
-
-	public:
-
-		valve_station& felix() { return stations.at(0); }
-		valve_station& james() { return stations.at(0); }
-		valve_station& lucas() { return stations.at(0); }
-
-
-		inline static landscape& instance() {
-			if (!singleton_instance.has_value()) {
-				singleton_instance.emplace();
-			}
-			return singleton_instance.value();
-		}
-
-	};
-
-}
-
-
 //static uint8_t james_valves{ 0 };
 
 void send_mayson(uint8_t auto_on = 2, uint8_t system_on = 2, uint8_t manual_on = 2) {
@@ -238,6 +203,195 @@ void send_valves(const std::string& ip_address, uint8_t valves, bool enable_log 
 		std::cout << r.text << std::endl;
 	}
 }
+
+
+
+namespace k1 {
+
+	/*
+		class valve {
+			std::string label;
+			uint8_t index_intra_valve_sation;
+
+		public:
+			valve(const std::string& _label, uint8_t _valve_bit_mask) : label(_label), index_intra_valve_sation(_valve_bit_mask) {}
+
+		};
+	*/
+
+
+	class valve_station {
+		std::string label;
+		std::string ip_address;
+		std::vector<std::string> valves;
+		std::vector<std::chrono::duration<double, std::milli>> accumulated_ms;
+		//std::vector<std::chrono::time_point<std::chrono::steady_clock>> last_switching_time;
+		std::chrono::time_point<std::chrono::steady_clock> last_switching_time;
+
+		uint8_t valves_state;
+
+		friend class landscape;
+		friend class valve_view;
+
+
+		void send(bool enable_log = true) {
+			send_valves(ip_address, valves_state, enable_log);
+		}
+
+		void turn(bool on, uint8_t valve_bit_mask) {
+			const uint8_t old_state{ valves_state };
+			if (on)
+				valves_state |= valve_bit_mask;
+			else
+				valves_state &= ~valve_bit_mask;
+			send();
+			const auto now = std::chrono::steady_clock::now();
+			for (uint8_t i = 0; i < accumulated_ms.size(); ++i) {
+				if (old_state & (static_cast<uint8_t>(1) << i)) {
+					accumulated_ms[i] += now - last_switching_time;
+				}
+			}
+			last_switching_time = now;
+		}
+
+		valve_station(const std::string& _label, const std::string& _ip_address, std::vector<std::string>&& _valves) :
+			label(_label),
+			ip_address(_ip_address),
+			valves(std::move(_valves)),
+			valves_state(0)
+		{
+			accumulated_ms = std::vector<std::chrono::duration<double, std::milli>>(valves.size(), std::chrono::duration<double, std::milli>(0.0));
+			//last_switching_time = std::vector<std::chrono::time_point<std::chrono::steady_clock>>(valves.size(), std::chrono::steady_clock::now());
+			last_switching_time = std::chrono::steady_clock::now();
+		}
+
+	public:
+
+		valve_station(valve_station&&) = default;
+		valve_station(const valve_station&) = delete;
+
+		valve_station& operator = (const valve_station&) = delete;
+		valve_station& operator = (valve_station&&) = delete;
+
+
+		class valve_view {
+			valve_station& station;
+			uint8_t valve_bit_mask;
+
+			valve_view(valve_station& _station, uint8_t _valve_bit_mask) : station(_station), valve_bit_mask(_valve_bit_mask) {
+
+			}
+			friend class valve_station;
+
+		public:
+			valve_view(const valve_view&) = default;
+			valve_view(valve_view&&) = default;
+
+
+			inline void turn(bool on) const {
+				return station.turn(on, valve_bit_mask);
+			}
+
+			inline void turn_on() const {
+				return turn(true);
+			}
+
+			inline void turn_off() const {
+				return turn(false);
+			}
+
+		};
+
+		valve_view get_view(uint8_t valve_id) {
+			(void)valves.at(valve_id); // check out of range!
+			return valve_view(*this, static_cast<uint8_t>(1) << valve_id);
+		}
+
+	};
+
+
+
+	class landscape {
+		std::vector<valve_station> stations;
+		static std::optional<landscape> singleton_instance;
+
+
+		landscape() {
+			standard_logger()->info("Creating Landscape...");
+
+			standard_logger()->info("Creating Felix...");
+			stations.push_back(valve_station(CONF::FELIX, IP_ADDRESS_VALVE_SERVER_FELIX, std::vector<std::string>{ CONF::FELIX_VALVE_1_LABEL, CONF::FELIX_VALVE_2_LABEL}));
+
+			standard_logger()->info("Creating James...");
+			stations.push_back(valve_station(CONF::JAMES, IP_ADDRESS_VALVE_SERVER_JAMES, std::vector<std::string>{ CONF::JAMES_VALVE_1_LABEL, CONF::JAMES_VALVE_2_LABEL, CONF::JAMES_VALVE_3_LABEL, CONF::JAMES_VALVE_4_LABEL}));
+
+			standard_logger()->info("Creating Lucas...");
+			stations.push_back(valve_station(CONF::LUCAS, IP_ADDRESS_VALVE_SERVER_LUCAS, std::vector<std::string>{ CONF::LUCAS_VALVE_1_LABEL, CONF::LUCAS_VALVE_2_LABEL, CONF::LUCAS_VALVE_3_LABEL}));
+		}
+
+	public:
+
+		class felix_view {
+			valve_station& station;
+
+			friend class landscape;
+
+			felix_view(valve_station& _station) : station(_station){}
+
+		public:
+
+			valve_station::valve_view Eiben() { return station.get_view(FELIX_EIBEN); }
+			valve_station::valve_view MaraSabine() { return station.get_view(FELIX_MARA); }
+		};
+
+		class james_view {
+			valve_station& station;
+
+			friend class landscape;
+
+			james_view(valve_station& _station) : station(_station) {}
+
+		public:
+
+			valve_station::valve_view Karotten() { return station.get_view(JAMES_KAROTTEN); }
+			valve_station::valve_view Gurken() { return station.get_view(JAMES_GURKEN); }
+			valve_station::valve_view Tomaten() { return station.get_view(JAMES_TOMATEN); }
+			valve_station::valve_view Frei() { return station.get_view(JAMES_FREI); }
+		
+		};
+
+		class lucas_view {
+			valve_station& station;
+
+			friend class landscape;
+
+			lucas_view(valve_station& _station) : station(_station) {}
+
+		public:
+
+			valve_station::valve_view FliederErdbeeren() { return station.get_view(LUCAS_VALVE_1); }
+			valve_station::valve_view Heidelbeeren() { return station.get_view(LUCAS_VALVE_2); }
+			valve_station::valve_view Kartoffeln() { return station.get_view(LUCAS_VALVE_3); }
+		
+		};
+
+
+		felix_view Felix() { return felix_view(stations.at(0)); }
+		james_view James() { return james_view(stations.at(1)); }
+		lucas_view Lucas() { return lucas_view(stations.at(2)); }
+		
+
+		inline static landscape& instance() {
+			if (!singleton_instance.has_value()) {
+				singleton_instance = std::make_optional<landscape>(landscape::landscape());
+			}
+			return singleton_instance.value();
+		}
+
+	};
+
+}
+
 
 
 void wait_for(int64_t duration_in_seconds) {
