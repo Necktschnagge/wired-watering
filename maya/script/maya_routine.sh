ee734ccb8aa1368617f63d6107094042b2e422c1
Maximilian Starke <maximilian.starke@medionmail.com>
2024-06-04 16:31:31 +0200

    minor changes

--- maya/script/maya_routine.sh
+++ maya/script/maya_routine.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#
-#
+
+
+
 #  TODOS
 #  files for commit: shell script, run_counter file, 6 log of prev run and 1..5 of current run. (?)
 #			-->>  check why uploading to artifacts branch failed
 #
-#
-# try to remove ; \ where not needed
 
 
 ############################################### check root ############################################################
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo >&2 "Error: script not running as root or with sudo! Exiting..."
+  echo >&2 "Error: script not running as root! Try sudo! Exiting..."
   exit 1
 fi
 
@@ -26,9 +25,10 @@ current_sleep_time_s=2400		#standard: 900 (15min), 1200 (20min), 1800 (30min), 2
 debug_step_sleep_s=0			#productive: 0 (no sleeps), debug: 15 (make log readable while running)
 
 logs_path="../../../logs" ##### take a dir outside the repository (second clone to update logs to artifacts branch....)
+entire_logs_path="../../../entire_logs" ##### take a dir outside the repository (second clone to update logs to artifacts branch....)
 #logs_path="../../../logs/maya/artifacts/logs/" ##### take a dir outside the repository (second clone to update logs to artifacts branch....)
-user_home="/home/mayadm"
-run_counter_file_path="${user_home}/repos/logs/counter.info"
+repository_path="~/wired-watering"
+run_counter_file_path="${logs_path}/counter.info"
 
 run_counter=0
 
@@ -48,6 +48,11 @@ log_file_name_prefix="${logs_path}/${run_counter}--${log_timestamp}--"
 
 
 	(
+		echo "> sudo -u mayadm mkdir ${entire_logs_path}"
+				sudo -u mayadm mkdir ${entire_logs_path}
+		echo "> sudo -u mayadm mkdir ${logs_path}"
+				sudo -u mayadm mkdir ${logs_path}
+		
 		echo "====================================================================================================="
 		echo "====================================================================================================="
 		echo "===     Starting with run #${run_counter}..."
@@ -229,16 +234,6 @@ log_file_name_prefix="${logs_path}/${run_counter}--${log_timestamp}--"
 		start_time=$(date +%s)
 		sleep ${debug_step_sleep_s}s
 
-#this does not work due to locality of variables:
-
-###		if [ "$build_success" = true ] ; then
-###			#######add timeout here when sleep mode implies valve shutdown.
-###			echo "> sudo -u mayadm ./ur-unix-run.sh"; \
-###					sudo -u mayadm ./ur-unix-run.sh 
-###			########use timeout XXXs ./run # 4 hours or so...
-###		else
-###			echo "FATAL! BUILD FAILED IN PREVIOUS STEP. WILL NOT EXECUTE!"
-###		fi
 		echo ">Please note:  Even if all builds failed, the last built executable is still there and will be started!"
 		echo "> sudo -u mayadm ./ur-unix-run.sh"
 				sudo -u mayadm ./ur-unix-run.sh
@@ -287,9 +282,19 @@ delete_branches=false
 		echo "====================================================================================================="
 	) 2>&1 | sudo -u mayadm tee ${log_file_name_prefix}-5.log
 	
-	echo "> sudo -u mayadm cp -n ${logs_path} ../artifacts/ --recursive"
-			sudo -u mayadm cp -n ${logs_path} ../artifacts/ --recursive # -n do not copy if file already present
+	echo "> sudo -u mayadm rm ../artifacts/*.log"
+			sudo -u mayadm rm ../artifacts/*.log
+
+	echo "> sudo -u mayadm cp ${logs_path} ../artifacts/ --recursive"
+			sudo -u mayadm cp ${logs_path} ../artifacts/ --recursive
 			# if you copy inside a tee-tracked block, in most cases an empty file wil be copied, commited and the changes will not be copied on the next run.
+	
+	echo "> sudo -u mayadm cp -n ${logs_path} ${entire_logs_path} --recursive" # -n do not copy if file already present
+			sudo -u mayadm cp -n ${logs_path} ${entire_logs_path} --recursive
+
+	echo "> sudo -u mayadm rm ${logs_path}/*.log"
+			sudo -u mayadm rm ${logs_path}/*.log
+
 	(
 		echo "====================================================================================================="
 		echo "%%%%%%%%%%     [6] Uploading logs and go sleeping..."
